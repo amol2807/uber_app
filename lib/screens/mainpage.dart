ff702de6aa7d6ae846b96888fb92fa7d30bbfc3f
Amol2807 <amoldgandhi@gmail.com>
2020-11-18 21:32:41 +0530

    4th Module Includes Search Sheet, Google Maps, Navigation Drawer // Imp: Scaffold Key (Understanding), Stack Layout

--- lib/screens/mainpage.dart
+++ lib/screens/mainpage.dart
@@ -1,8 +1,14 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:outline_material_icons/outline_material_icons.dart';
+import 'package:uber_app/Widgets/BrandDivider.dart';
+import 'package:uber_app/brand_colors.dart';
+import 'package:uber_app/styles/styles.dart';
 
 class MainPage extends StatefulWidget {
-
-
   static const String id = 'mainpage';
 
   @override
@@ -10,13 +16,307 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  Completer<GoogleMapController> _controller = Completer();
+  GoogleMapController mapController;
+  double mapBottomPadding = 0;
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
+      key: scaffoldKey,
+      drawer: Container(
+        width: 250.0,
+        color: Colors.white,
+        child: Drawer(
+          child: ListView(
+            padding: EdgeInsets.all(0),
+            children: <Widget>[
+              Container(
+                height: 160.0,
+                color: Colors.white,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Image(
+                        height: 60.0,
+                        width: 60.0,
+                        image: AssetImage('images/user_icon.png'),
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'Amol',
+                            style: TextStyle(
+                                fontSize: 20, fontFamily: 'Brand-Bold'),
+                          ),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          Text('View Profile'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              BrandDivider(),
 
-      appBar: AppBar(
-        title: Text('Main Page'),
+              SizedBox(
+                height: 10.0,
+              ),
+
+              ListTile(
+                leading: Icon(OMIcons.cardGiftcard),
+                title: Text(
+                  'Free Rides',
+                  style: kDrawerItemStyle,
+                ),
+              ), // Free Rides
+              ListTile(
+                leading: Icon(OMIcons.creditCard),
+                title: Text(
+                  'Payments',
+                  style: kDrawerItemStyle,
+                ),
+              ), // Payments
+              ListTile(
+                leading: Icon(OMIcons.history),
+                title: Text(
+                  'Ride History',
+                  style: kDrawerItemStyle,
+                ),
+              ), //Ride History
+              ListTile(
+                leading: Icon(OMIcons.contactSupport),
+                title: Text(
+                  'Support',
+                  style: kDrawerItemStyle,
+                ),
+              ), //
+              ListTile(
+                leading: Icon(OMIcons.info),
+                title: Text(
+                  'About',
+                  style: kDrawerItemStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            padding: EdgeInsets.only(bottom: mapBottomPadding),
+            mapType: MapType.normal,
+            myLocationButtonEnabled: true,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+              mapController = controller;
+
+              setState(() {
+                mapBottomPadding = 275;
+              });
+            },
+          ),
+
+          //Menu Button
+          Positioned(
+            top: 44,
+            left: 20,
+            child: GestureDetector(
+              onTap: () {
+                scaffoldKey.currentState.openDrawer();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 5.0,
+                      spreadRadius: 0.5,
+                      offset: Offset(0.7, 0.7),
+                    ),
+                  ],
+                ),
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 20.0,
+                  child: Icon(
+                    Icons.menu,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // Search Sheet
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 275.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15.0),
+                  topRight: Radius.circular(15.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 15.0,
+                    spreadRadius: 0.6,
+                    offset: Offset(0.7, 0.7),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 24.0, vertical: 18.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Text(
+                      'Nice to see you!',
+                      style: TextStyle(
+                        fontSize: 10.0,
+                      ),
+                    ),
+                    Text(
+                      'Where are you going?',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontFamily: 'Brand-Bold',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(4),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 5.0,
+                            spreadRadius: 0.5,
+                            offset: Offset(
+                              7.0,
+                              7.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.search,
+                              color: Colors.blueAccent,
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            Text('Search Destination'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 22.0,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          OMIcons.home,
+                          color: BrandColors.colorDimText,
+                        ),
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text('Add Home'),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              'Your residential address',
+                              style: TextStyle(
+                                fontSize: 11.0,
+                                color: BrandColors.colorDimText,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    BrandDivider(),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          OMIcons.workOutline,
+                          color: BrandColors.colorDimText,
+                        ),
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text('Add Work'),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              'Your office address',
+                              style: TextStyle(
+                                fontSize: 11.0,
+                                color: BrandColors.colorDimText,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
